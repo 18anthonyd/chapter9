00e51973cf28234f403d72aa665b9fec136a647d
Dominic <18anthonyd@badinhs.org>
2017-01-26 08:03:42 -0500

    Initial Commit

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by BHSRam6 on 1/26/17.
+//  Copyright © 2017 BHSRam6. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet var nameLabel: UIlabel!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
